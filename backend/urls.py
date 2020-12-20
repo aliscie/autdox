9446feac51b65adad54caf3b8ec650b35ddc18de
ali <weplutus.1@gmail.com>
2020-12-20 13:26:33 +0300

    1. show the loged in user 2. show login/sign_up link not loged in 3. make a user link. 4. make readme

--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,8 @@ from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
+    path('accounts/profile/<str:user>/', index, name='index'),
+    path('accounts/profile/', index, name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 ]
--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ function App() {
   return (
     <div className="App">
       <h1>{element.dataset.username}</h1>
+      {element.dataset.username === "AnonymousUser" && (
+        // 🔴 how to login/singup without leaving the page.
+        <a href="https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=13721924041-tnmmge21adu3mg7k7agvcgiqs7f2trnt.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Faccounts%2Fgoogle%2Flogin%2Fcallback%2F&scope=profile&response_type=code&state=TOH6guRryjRf&flowName=GeneralOAuthFlow">
+          login
+        </a>
+      )}
+
       <header className="App-header">
         <h1>ajax authenticated name should be here.</h1>
       </header>
